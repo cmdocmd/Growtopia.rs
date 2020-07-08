1fec4e465ca0e4c77ac6e142b5a27e655835100b
cmdocmd <55096480+cmdocmd@users.noreply.github.com>
2020-07-09 03:56:28 +0400

    Update gamepacket.rs

--- src/handler/gamepacket.rs
+++ src/handler/gamepacket.rs
@@ -64,7 +64,7 @@ impl GamePacket {
     pub fn intx(mut self, int: i64) -> Self {
     self.data.put_uint_le(self.index as u64, 1);
     self.data.put_uint_le(0x5, 1);
-    self.data.put_int_le(int, 4);
+    self.data.put_uint_le(int, 4);
 
     self.index += 1;
     self.len = self.data.len();
